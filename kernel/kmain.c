e955cb64dc95c636150f1074c6edadf24e5e234c
Kris Foster <kris.foster@gmail.com>
2020-11-28 12:40:02 -0800

    Add some debugging output

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -37,6 +37,7 @@ typedef struct {
     uint16_t sr;        // Status register (and really just CCR)
     uint32_t a[8];      // Address registers, 8 is usp
     uint32_t d[8];      // Data registers
+    bool running;       // temporary process state
 } context_t;
 
 typedef struct {
@@ -45,11 +46,9 @@ typedef struct {
     context_t context;
 } process_t;
 
-int process_list;
 context_t *current_process;
 context_t *process_a;
 context_t *process_b;
-bool ready;
 
 void tick_handler();
 
@@ -58,10 +57,6 @@ void user_routine_b();
 
 noreturn void kmain() {
   debug_stub();
-
-  ready = false;
-
-  process_list = 0; // 0 = A, 1 = B
 
   for (int i=0;i<50000;i++) {/* do nothing for a while */}
 
@@ -73,89 +68,64 @@ noreturn void kmain() {
   context_t context_a = {
     .usp = 0x4000,
     .pc = (uint32_t)user_routine_a,
-    .sr = 0x0
+    .sr = 0x0,
+    .a = {0,0,0,0,0,0,0,0},
+    .d = {0,0,0,0,0,0,0,0},
+    .running = false
   };
   process_a = &context_a;
 
   context_t context_b = {
     .usp = 0x6000,
     .pc = (uint32_t)user_routine_b,
-    .sr = 0x0
+    .sr = 0x0,
+    .a = {0,0,0,0,0,0,0,0},
+    .d = {0,0,0,0,0,0,0,0},
+    .running = false
   };
   process_b = &context_b;
 
   // Start the scheduler
   SET_VECTOR(tick_handler, MFP_TIMER_C);
 
-  //disable_supervisor();
-
   sleep();
 }
 
 /*
 Interrupt handler for counting every time Timer C fires
+
+-> SSP-6 Status Register
+   SSP-4 Program Counter (High)
+   SSP-2 Program Counter (Low)
+   SSP (Pre-exception)
+
 */
 void __attribute__ ((interrupt)) tick_handler() {
-  INC_LONG(SYS_TICKS);        // Count the ticks of the timer
+  // Should these just be globals in an include?
+
+  //INC_LONG(SYS_TICKS);        // Count the ticks of the timer
   //e68Println("Handling tick");
 
-  if (ready) {
-    // 1. Save PC (4B) and SR (2B) from stack into the old process context
-    __asm__ __volatile__ (
-      "move +2(%%a7),%0"
-      :"=g" (current_process->pc)::
-    );
+  // Switch processes (this will be a circular linked list next)
+  if (current_process == process_b) {
+    current_process = process_a;
+  } else {
+    current_process = process_b;
+  }
 
-    //e68PrintNumSignedWidth(current_process->pc, 10);
-    //e68Println("");
+  // Context is meaningless if the process hasn't started
+  if (current_process->running) {
+    // Save PC (4B) and SR (2B) from stack into the old process context
+    __asm__ __volatile__ (
+      "move.w +2(%%a7),%0" : "=g" (current_process->pc) ::
+    );
     
-    // 3. Save Address and Data registers to old process context
+    // Save Address and Data registers to old process context
     __asm__ __volatile__ (
-      "move %%a0,%0\n\t"
-      "move %%a1,%1\n\t"
-      "move %%a2,%2\n\t"
-      "move %%a3,%3\n\t"
-      "move %%a4,%4\n\t"
-      "move %%a5,%5\n\t"
-      "move %%a6,%6\n\t"
-      "move %%a7,%6"
-      :
-      "=g" (current_process->a[0]),
-      "=g" (current_process->a[1]),
-      "=g" (current_process->a[2]),
-      "=g" (current_process->a[3]),
-      "=g" (current_process->a[4]),
-      "=g" (current_process->a[5]),
-      "=g" (current_process->a[6]),
-      "=g" (current_process->a[0])
-      ::
+      "movem.l %%a0-%%a7/%%d0-%%d7,%0" : "=g" (current_process->a) ::
     );
 
-    //e68PrintNumSignedWidth(current_process->a[0], 10);
-    //e68Println("");
-
-    __asm__ __volatile__ (
-      "move %%d0,%0\n\t"
-      "move %%d1,%1\n\t"
-      "move %%d2,%2\n\t"
-      "move %%d3,%3\n\t"
-      "move %%d4,%4\n\t"
-      "move %%d5,%5\n\t"
-      "move %%d6,%6\n\t"
-      "move %%d7,%6"
-      :
-      "=g" (current_process->d[0]),
-      "=g" (current_process->d[1]),
-      "=g" (current_process->d[2]),
-      "=g" (current_process->d[3]),
-      "=g" (current_process->d[4]),
-      "=g" (current_process->d[5]),
-      "=g" (current_process->d[6]),
-      "=g" (current_process->d[0])
-      ::
-    );
-
-    // 2. Save USP to old process context
+    // Save USP to old process context
     __asm__ __volatile__ (
       "move %%usp,%%a0\n\t"
       "move %%a0,%0"
@@ -163,89 +133,91 @@ void __attribute__ ((interrupt)) tick_handler() {
     );
   }
 
-  //e68PrintNumSignedWidth(current_process->usp, 10);
-  //e68Println("");
+  e68Println(""); 
+  e68Println("Next process context");
+  e68Print("USP: 0x");
+  e68DisplayNumUnsigned(current_process->usp,16);
+  e68Println("");
+  e68Print("PC: 0x");
+  e68DisplayNumUnsigned(current_process->pc,16);
+  e68Println("");
+  e68Print("SR: 0x");
+  e68DisplayNumUnsigned(current_process->sr,16);
+  e68Println("");
 
-  // Switch processes (this will be a circular linked list next)
-  if (process_list == 1) {
-    current_process = process_a;
-    process_list = 0;
-    ready = true; // temporary hack
-    e68Println("Switching to process_a");
+  e68Print("A0:0x");
+  e68DisplayNumUnsigned(current_process->a[0],16);
+  e68Print(" A1:0x");
+  e68DisplayNumUnsigned(current_process->a[1],16);
+  e68Print(" A2:0x");
+  e68DisplayNumUnsigned(current_process->a[2],16);
+  e68Print(" A3:0x");
+  e68DisplayNumUnsigned(current_process->a[3],16);
+  e68Print(" A4:0x");
+  e68DisplayNumUnsigned(current_process->a[4],16);
+  e68Print(" A5:0x");
+  e68DisplayNumUnsigned(current_process->a[5],16);
+  e68Print(" A6:0x");
+  e68DisplayNumUnsigned(current_process->a[6],16);
+  e68Print(" A7:0x");
+  e68DisplayNumUnsigned(current_process->a[7],16);
+  e68Println("");
+  
+  e68Print("D0:0x");
+  e68DisplayNumUnsigned(current_process->d[0],16);
+  e68Print(" D1:0x");
+  e68DisplayNumUnsigned(current_process->d[1],16);
+  e68Print(" D2:0x");
+  e68DisplayNumUnsigned(current_process->d[2],16);
+  e68Print(" D3:0x");
+  e68DisplayNumUnsigned(current_process->d[3],16);
+  e68Print(" D4:0x");
+  e68DisplayNumUnsigned(current_process->d[4],16);
+  e68Print(" D5:0x");
+  e68DisplayNumUnsigned(current_process->d[5],16);
+  e68Print(" D6:0x");
+  e68DisplayNumUnsigned(current_process->d[6],16);
+  e68Print(" D7:0x");
+  e68DisplayNumUnsigned(current_process->d[7],16);
+  e68Println(""); 
+
+  e68Print("Running: ");
+  if (current_process->running) {
+    e68Println("True");
   } else {
-    process_list = 1;
-    current_process = process_b;
-    ready = true; // temporary hack
-    e68Println("Switching to process_b");
+    e68Println("False");
   }
 
-  // 5. Move new USP into USP
+  // Move new USP into USP
   register uint32_t *a0 __asm__ ("a0") __attribute__((unused));
-
   a0 = (uint32_t *)current_process->usp;
-  __asm__ __volatile__ (
-    "move %%a0,%%usp"
-    :::
-  );
+  __asm__ __volatile__ ("move %%a0,%%usp":::);
 
-  e68Println("User Stack Pointer set");
+  //e68Print("USP set to:");
+  //e68PrintNumSignedWidth(current_process->usp, 10);
+  //e68Println("");
 
   // 4. Replace PC and SR on stack w/ new process's values
   // Program counter is at stack pointer+2
   __asm__ __volatile__ (
-    "move.l %0,2(%%a7)"
+    "move.l %0,+2(%%a7)"  // I'm not convinced this offset is right
     ::"g" (current_process->pc):
   );
 
-  e68Println("Program Counter set");
+  //e68Print("PC set to:");
+  //e68PrintNumSignedWidth(current_process->pc, 10);
+  //e68Println("");
 
   // 6. Restore the A, D registers
-  __asm__ __volatile__ (
-    "move.w %0,%%a0\n\t"
-    "move.w %1,%%a1\n\t"
-    "move.w %2,%%a2\n\t"
-    "move.w %3,%%a3\n\t"
-    "move.w %4,%%a4\n\t"
-    "move.w %5,%%a5\n\t"
-    "move.w %6,%%a6\n\t"
-    "move.w %7,%%a7\n\t"
-    ::
-    "g" (current_process->a[0]),
-    "g" (current_process->a[1]),
-    "g" (current_process->a[2]),
-    "g" (current_process->a[3]),
-    "g" (current_process->a[4]),
-    "g" (current_process->a[5]),
-    "g" (current_process->a[6]),
-    "g" (current_process->a[7])
-    :
-  );
-
-  __asm__ __volatile__ (
-    "move.w %0,%%d0\n\t"
-    "move.w %1,%%d1\n\t"
-    "move.w %2,%%d2\n\t"
-    "move.w %3,%%d3\n\t"
-    "move.w %4,%%d4\n\t"
-    "move.w %5,%%d5\n\t"
-    "move.w %6,%%d6\n\t"
-    "move.w %7,%%d7\n\t"
-    ::
-    "g" (current_process->d[0]),
-    "g" (current_process->d[1]),
-    "g" (current_process->d[2]),
-    "g" (current_process->d[3]),
-    "g" (current_process->d[4]),
-    "g" (current_process->d[5]),
-    "g" (current_process->d[6]),
-    "g" (current_process->d[7])
-    :
-  );
-
+  //__asm__ __volatile__ (
+  //  "movem.l %0,%%a0-%%a7/%%d0-%%d7"
+  //  ::"g" (current_process->a):
+  //);
+  current_process->running = true;
   // 7. Clear interrupt-in-service
   SET_BYTE(~0x20, MFP_ISRB);  // Clear interrupt-in-service
 
-  // 8. RTE
+  // RTE implied by interrupt attribute on function
 }
 
 /*
