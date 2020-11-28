7eecc7de0b7fd17ba38000051455185597f0a83e
Kris Foster <kris.foster@gmail.com>
2020-11-27 21:33:49 -0800

    jmp to our process rather than call a function

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -27,6 +27,10 @@ SOFTWARE.
 #include "system.h"
 #include "mfp.h"
 
+typedef uint8_t state;
+#define RUNNING 0
+#define DEAD 1
+
 typedef struct {
     uint32_t usp;       // User stack pointer
     uint32_t pc;        // Program counter
@@ -37,14 +41,17 @@ typedef struct {
 
 typedef struct {
     uint16_t pid;       // Process ID
+    state state;
     context_t context;
 } process_t;
+
+void save_context(context_t *context);
+void set_context(context_t *context);
 
 void tick_handler();
 
 void user_routine_a();
 void user_routine_b();
-
 
 noreturn void kmain() {
   debug_stub();
@@ -58,16 +65,40 @@ noreturn void kmain() {
 
   for (int i=0;i<50000;i++) {/* do nothing for a while */}
 
-  context_t process_a = {
+  context_t context_a = {
     .usp = 0x4000,
     .pc = (uint32_t)user_routine_a,
     .sr = 0x0
   };
-  set_usp(process_a.usp);
+  set_context(&context_a);
   disable_supervisor();
-  user_routine_a();
+  __asm__ __volatile (
+    "move.w %0,%%a0\n\t"
+    "jmp (%%a0)"
+    : : "g" (context_a.pc) :
+  );
 
   sleep();
+}
+
+/*
+Save the current process's context
+*/
+void save_context(context_t *context) {
+
+}
+
+/*
+Set up the context for a new process
+*/
+void set_context(context_t *context) {
+    register uint32_t *a0 __asm__ ("a0") __attribute__((unused));
+
+    // Set the user stack pointer
+    a0 = (uint32_t *)context->usp;
+    __asm__ __volatile__ (
+        "move %%a0,%%usp":::"a0"
+    );
 }
 
 /*
@@ -75,8 +106,6 @@ Interrupt handler for counting every time Timer C fires
 */
 void __attribute__ ((interrupt)) tick_handler() {
   INC_LONG(SYS_TICKS);        // Count the ticks of the timer
-
-  e68Println("T");
 
   // Scheduler
   // 1. Save PC (4B) and SR (2B) from stack into the old process context
