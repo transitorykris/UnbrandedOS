8aecfe783f64c02cb78345859a526794f766b568
Kris Foster <kris.foster@gmail.com>
2021-01-01 17:41:35 -0800

    Remove the unused sentinels

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -60,23 +60,17 @@ noreturn void kmain() {
 
   // This context gets trashed after the first context switch  
   struct context_t throw_away = {
-    .d = {0xd1d1d1dd,0xd1d1d1dd,0x2d2d2ddd,0x3d3d3ddd,0x4d4d4ddd,0x5d5d5ddd,0x6d6d6ddd,0x7d7d7ddd},
-    .a = {0xd1d1d1aa,0xd1d1d1aa,0x2d2d2daa,0x3d3d3daa,0x4d4d4daa,0x5d5d5daa,0x6d6d6daa},
     .usp = 0x6000,
   };
   current_process = &throw_away;
 
   struct context_t pid1_context = {
-    .d = {0x010101dd,0x111111dd,0x212121dd,0x313131dd,0x414141dd,0x515151dd,0x616161dd,0x717171dd},
-    .a = {0x010101aa,0x111111aa,0x212121aa,0x313131aa,0x414141aa,0x515151aa,0x616161aa},
     .usp = 0x10000,
     .pc = (uint32_t)user_routine_a,
   };
   current_process->next = &pid1_context;
 
   struct context_t pid2_context = {
-    .d = {0x0f0f0fdd,0x1f1f1fdd,0x2f2f2fdd,0x3f3f3fdd,0x4f4f4fdd,0x5f5f5fdd,0x6f6f6fdd,0x7f7f7fdd},
-    .a = {0x0f0f0faa,0x1f1f1faa,0x2f2f2faa,0x3f3f3faa,0x4f4f4faa,0x5f5f5faa,0x6f6f6faa},
     .usp = 0x12000,
     .pc = (uint32_t)user_routine_b,
     .next = &pid1_context,
