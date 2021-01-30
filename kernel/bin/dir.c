58573ce9d8ca826c2da36e4ae549f87b67d2c4c9
Kris Foster <kris.foster@gmail.com>
2021-01-30 13:11:38 -0800

    A broken dir command on the SD

---
+++ kernel/bin/dir.c
@@ -0,0 +1,35 @@
+/*
+Copyright 2021 Kris Foster
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <machine.h>
+
+#include <sdfat.h>
+
+/* List directory */
+int dir(int argc, char *argv[]) {
+    //fl_listdirectory("/");
+    //SD_FAT_initialize();
+    //SDCard *sdcard = SD_FAT_get_sd_card();
+    printf("Size of card: %d\n\r", SD_get_size());
+
+    return 0;
+}
