a2b3e82b2ff1cd260982330bf6fedb36d39e7338
Kris Foster <kris.foster@gmail.com>
2021-02-15 16:43:56 -0800

    whitespace

--- kernel/sys/types.h
+++ kernel/sys/types.h
@@ -30,35 +30,35 @@ SOFTWARE.
 typedef uint16_t pid_t; // Used for process IDs and process group IDs.
 typedef uint16_t uid_t; // Used for user IDs.
 
-//blkcnt_t              //Used for file block counts
-//blksize_t             //Used for block sizes
-//clock_t               //Used for system times in clock ticks or CLOCKS_PER_SEC (see <time.h>).
-//clockid_t             //Used for clock ID type in the clock and timer functions.
-//dev_t                 //Used for device IDs.
-//fsblkcnt_t            //Used for file system block counts
-//fsfilcnt_t            //Used for file system file counts
-//gid_t                 //Used for group IDs.
-//id_t                  //Used as a general identifier; can be used to contain at least a pid_t, uid_t or a gid_t.
-//ino_t                 //Used for file serial numbers.
-//key_t                 //Used for interprocess communication.
-//mode_t                //Used for some file attributes.
-//nlink_t               //Used for link counts.
-//off_t                 //Used for file sizes.
-//pthread_attr_t        //Used to identify a thread attribute object.
-//pthread_cond_t        //Used for condition variables.
-//pthread_condattr_t    //Used to identify a condition attribute object.
-//pthread_key_t         //Used for thread-specific data keys.
-//pthread_mutex_t       //Used for mutexes.
-//pthread_mutexattr_t   //Used to identify a mutex attribute object.
-//pthread_once_t        //Used for dynamic package initialisation.
-//pthread_rwlock_t      //Used for read-write locks.
-//pthread_rwlockattr_t  //Used for read-write lock attributes.
-//pthread_t             //Used to identify a thread.
-//size_t                //Used for sizes of objects.
-//ssize_t               //Used for a count of bytes or an error indication.
-//suseconds_t           //Used for time in microseconds
-//time_t                //Used for time in seconds.
-//timer_t               //Used for timer ID returned by timer_create().
-//useconds_t            //Used for time in microseconds.
+//blkcnt_t              // Used for file block counts
+//blksize_t             // Used for block sizes
+//clock_t               // Used for system times in clock ticks or CLOCKS_PER_SEC (see <time.h>).
+//clockid_t             // Used for clock ID type in the clock and timer functions.
+//dev_t                 // Used for device IDs.
+//fsblkcnt_t            // Used for file system block counts
+//fsfilcnt_t            // Used for file system file counts
+//gid_t                 // Used for group IDs.
+//id_t                  // Used as a general identifier; can be used to contain at least a pid_t, uid_t or a gid_t.
+//ino_t                 // Used for file serial numbers.
+//key_t                 // Used for interprocess communication.
+//mode_t                // Used for some file attributes.
+//nlink_t               // Used for link counts.
+//off_t                 // Used for file sizes.
+//pthread_attr_t        // Used to identify a thread attribute object.
+//pthread_cond_t        // Used for condition variables.
+//pthread_condattr_t    // Used to identify a condition attribute object.
+//pthread_key_t         // Used for thread-specific data keys.
+//pthread_mutex_t       // Used for mutexes.
+//pthread_mutexattr_t   // Used to identify a mutex attribute object.
+//pthread_once_t        // Used for dynamic package initialisation.
+//pthread_rwlock_t      // Used for read-write locks.
+//pthread_rwlockattr_t  // Used for read-write lock attributes.
+//pthread_t             // Used to identify a thread.
+//size_t                // Used for sizes of objects.
+//ssize_t               // Used for a count of bytes or an error indication.
+//suseconds_t           // Used for time in microseconds
+//time_t                // Used for time in seconds.
+//timer_t               // Used for timer ID returned by timer_create().
+//useconds_t            // Used for time in microseconds.
 
 #endif
